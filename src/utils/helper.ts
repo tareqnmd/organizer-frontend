dbea60b8384660379af362f0f51dc3273c5da866
tareqnmd <tareqnmd@gmail.com>
2023-05-13 08:49:51 +0600

    update data

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,6 +1,27 @@
+import { format } from 'date-fns';
 import { GridColumnProp } from './common-types';
 
 export const getFieldValue = (item: any, column: GridColumnProp) => {
 	const dataIndex = column.dataIndex;
-	return item[dataIndex];
+	const modifiedValue =
+		column.type === 'number'
+			? millionNumberFormat(item[dataIndex])
+			: column.type === 'date'
+			? dateFormat(item[dataIndex])
+			: item[dataIndex];
+	return modifiedValue;
+};
+
+export const dateFormat = (date: Date) => {
+	return format(new Date(date), 'dd-MM-yyyy');
+};
+
+export const millionNumberFormat = (
+	amount: number,
+	currency = 'BDT',
+	format = 'en-US'
+) => {
+	const number = Number(amount.toFixed(2));
+	const result = Intl.NumberFormat(format).format(number);
+	return `${result} ${currency}`;
 };
