4b7e4aab504b83ec73b3d61a6312c2092173e560
Tareq <tareqnmd@gmail.com>
2023-06-28 11:51:40 +0600

    update data

--- components/auth/AuthTopInfo.tsx
+++ components/auth/AuthTopInfo.tsx
@@ -4,7 +4,7 @@ const AuthTopInfo = ({ type }: { type: string }) => {
 	return (
 		<div className="text-center mb-4">
 			<PrimaryLogo />
-			<h3 className="mt-3 text-xl font-medium">Welcome Back</h3>
+			<h3 className="mt-3 text-xl font-medium">Welcome</h3>
 			<p className="mt-1 text-gray-500">
 				{type === 'login' ? 'Login' : 'Create'} your account
 			</p>
