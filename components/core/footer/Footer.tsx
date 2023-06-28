21e056c9da83b1c7438044cade392b5952a83cc5
Tareq <tareqnmd@gmail.com>
2023-06-28 12:26:13 +0600

    update data

--- components/core/footer/Footer.tsx
+++ components/core/footer/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <footer className="flex items-center justify-center p-3 border-t-2">
+        <footer className="flex items-center justify-center p-3 border-t-2 bg-white">
             Developed by <a className="ml-1 text-blue-600" target="_blank" href="https://tareqnmd.com/">Tareqnmd</a>
         </footer>
     );
--- components/transactions/TransactionInfo.tsx
+++
@@ -1,7 +0,0 @@
-import { ITransaction } from '@/utils/types/transaction-types';
-
-const TransactionInfo = ({ transaction }: { transaction: ITransaction }) => {
-	return <></>;
-};
-
-export default TransactionInfo;
