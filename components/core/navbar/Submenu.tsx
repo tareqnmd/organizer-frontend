14250e67f95f41422241987186d5f6859ef672d5
Tareq <tareqnmd@gmail.com>
2023-10-12 10:13:21 +0600

    container for sub menu

--- components/core/navbar/Submenu.tsx
+++ components/core/navbar/Submenu.tsx
@@ -5,24 +5,26 @@ import { usePathname } from 'next/navigation';
 const Submenu = ({ links }: any) => {
 	const pathname = usePathname();
 	return (
-		<header className="flex items-center justify-end gap-4 p-2 bg-[#dfdfdf]">
-			{links?.map((link: any, index: number) => (
-				<Link
-					key={index}
-					href={link?.path}
-					className={
-						(
-							link?.exact
-								? pathname === link?.path
-								: pathname.includes(link?.path)
-						)
-							? 'active-path'
-							: ''
-					}
-				>
-					<span>{link?.name}</span>
-				</Link>
-			))}
+		<header className="bg-[#dfdfdf]">
+			<div className="container py-3 px-2 m-auto flex items-center justify-end gap-4">
+				{links?.map((link: any, index: number) => (
+					<Link
+						key={index}
+						href={link?.path}
+						className={
+							(
+								link?.exact
+									? pathname === link?.path
+									: pathname.includes(link?.path)
+							)
+								? 'active-path'
+								: ''
+						}
+					>
+						<span>{link?.name}</span>
+					</Link>
+				))}
+			</div>
 		</header>
 	);
 };
