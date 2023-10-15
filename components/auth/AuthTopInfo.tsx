3cdbd6142c22f1497706cc7f79d441ea5bac390f
Tareq <tareqnmd@gmail.com>
2023-10-15 16:27:22 +0600

    update login

--- components/auth/AuthTopInfo.tsx
+++ components/auth/AuthTopInfo.tsx
@@ -2,7 +2,7 @@ import PrimaryLogo from '../ui/logo/PrimaryLogo';
 
 const AuthTopInfo = ({ type }: { type: string }) => {
 	return (
-		<div className="text-center mb-4">
+		<div className="flex flex-col items-center mb-4">
 			<PrimaryLogo />
 			<h3 className="mt-3 text-xl font-medium">Welcome</h3>
 			<p className="mt-1 text-gray-500">
