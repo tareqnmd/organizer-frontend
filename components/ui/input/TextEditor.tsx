0883527ffd59eca4edda0c67945e12adfbe15a50
Tareq <tareqnmd@gmail.com>
2023-10-26 15:48:46 +0600

    update quill document

--- components/ui/input/TextEditor.tsx
+++ components/ui/input/TextEditor.tsx
@@ -1,9 +1,9 @@
 import { quillEditorOptions } from '@/utils/helpers/ui';
 import { Controller } from 'react-hook-form';
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
 
 const TextEditor = ({ input, control }: any) => {
+	const ReactQuill =
+		typeof window === 'object' ? require('react-quill') : () => false;
 	const { name } = input;
 	return (
 		<Controller
