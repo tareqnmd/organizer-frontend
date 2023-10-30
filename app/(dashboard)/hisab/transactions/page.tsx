f272653e45f88577159e7203421ddc49e11e9a04
Tareq <tareqnmd@gmail.com>
2023-10-31 00:25:13 +0600

    old one

--- app/(dashboard)/hisab/transactions/page.tsx
+++ app/(dashboard)/hisab/transactions/page.tsx
@@ -1,18 +1,7 @@
-import Transactions from '@/components/hisab/transactions/Transactions';
-import { fetchServerData } from '@/utils/fetch/axios-fetch';
-async function getTransactionData() {
-	try {
-		const response = await fetchServerData('transaction', {
-			month: new Date().getMonth() + 1,
-		});
-		return response.data;
-	} catch (error) {
-		return [];
-	}
-}
-const page = async () => {
-	const data = await getTransactionData();
-	return <Transactions data={data} />;
+import Transactions from "@/components/hisab/transactions/Transactions";
+
+const page = () => {
+	return <Transactions />;
 };
 
 export default page;
--- components/hisab/transactions/Transactions.tsx
+++ components/hisab/transactions/Transactions.tsx
@@ -1,7 +1,20 @@
+import { fetchServerData } from '@/utils/fetch/axios-fetch';
 import TransactionsDetails from '../transactions/TransactionsDetails';
 
-const Transactions = async ({ data }: any) => {
-	return <TransactionsDetails transactions={data} />;
+async function getTransactionData() {
+	try {
+		const response = await fetchServerData('transaction', {
+			month: new Date().getMonth() + 1,
+		});
+		return response.data;
+	} catch (error) {
+		return [];
+	}
+}
+
+const Transactions = async () => {
+	const transactions = await getTransactionData();
+	return <TransactionsDetails transactions={transactions} />;
 };
 
 export default Transactions;
