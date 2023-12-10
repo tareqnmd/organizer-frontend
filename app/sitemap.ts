9200d5f40cfdef13700590fb87eddc7d4917ee42
Tareq <tareqnmd@gmail.com>
2023-12-10 19:42:35 +0600

    update files

---
+++ app/sitemap.ts
@@ -0,0 +1,30 @@
+import { MetadataRoute } from 'next';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+	return [
+		{
+			url: 'https://organizer.vercel.com',
+			lastModified: new Date(),
+			changeFrequency: 'yearly',
+			priority: 1,
+		},
+		{
+			url: 'https://organizer.vercel.com/login',
+			lastModified: new Date(),
+			changeFrequency: 'monthly',
+			priority: 0.8,
+		},
+		{
+			url: 'https://organizer.vercel.com/hisab',
+			lastModified: new Date(),
+			changeFrequency: 'weekly',
+			priority: 0.5,
+		},
+		{
+			url: 'https://organizer.vercel.com/note',
+			lastModified: new Date(),
+			changeFrequency: 'weekly',
+			priority: 0.5,
+		},
+	];
+}
