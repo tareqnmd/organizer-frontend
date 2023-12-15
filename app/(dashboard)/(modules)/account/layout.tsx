50bd3a2f390768eb2bde5c639b348c2a1113426a
Tareq <tareqnmd@gmail.com>
2023-12-15 17:58:30 +0600

    update

--- app/(dashboard)/(modules)/account/layout.tsx
+++ app/(dashboard)/(modules)/account/layout.tsx
@@ -1,8 +1,8 @@
 import ModuleLayout from '@/components/provider/ModuleLayout';
 const links = [
 	{ name: 'Transactions', path: '/account/transactions' },
-	{ name: 'Create', path: '/account/create' },
-	{ name: 'Types', path: '/account/types' },
+	{ name: 'Create', path: '/account/transaction-create' },
+	{ name: 'Types', path: '/account/transaction-types' },
 ];
 const Layout = ({ children }: { children: React.ReactNode }) => {
 	return <ModuleLayout links={links}>{children}</ModuleLayout>;
