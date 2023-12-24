7cd1560ac12589d2afb2a900b68e32268c25a564
Tareq <tareqnmd@gmail.com>
2023-12-24 23:34:36 +0600

    updated

--- components/modules/Note/NotesAction.tsx
+++ components/modules/Note/NotesAction.tsx
@@ -2,9 +2,15 @@ import Link from 'next/link';
 
 const NotesAction = () => {
 	return (
-		<div className="flex item-center justify-end mb-3">
+		<div className="flex item-center justify-end gap-2 mb-3">
 			<Link
-				className="border rounded px-2"
+				className="border rounded px-3 py-1 text-sm"
+				href="/note/all"
+			>
+				All
+			</Link>
+			<Link
+				className="border rounded px-3 py-1 text-sm"
 				href="/note/all?pinned=1"
 			>
 				Pinned
