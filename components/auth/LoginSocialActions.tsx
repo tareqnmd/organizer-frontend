9de05922e59bb580c85f8ed4946ccecdbd0a6e91
Tareq <tareqnmd@gmail.com>
2024-01-10 15:04:53 +0600

    update social login

--- components/auth/LoginSocialActions.tsx
+++ components/auth/LoginSocialActions.tsx
@@ -5,26 +5,23 @@ import Google from '../icon/GoogleIcon';
 import { Button } from '../ui/button';
 
 const LoginSocialActions = () => {
+	const socialLogin = async (type: string) => {
+		await signIn(type, {
+			redirect: false,
+		});
+	};
 	return (
 		<div className="grid grid-cols-2 gap-2  w-full">
 			<Button
 				className="gap-2"
-				onClick={() =>
-					signIn('google', {
-						callbackUrl: `${window.location.origin}`,
-					})
-				}
+				onClick={() => socialLogin('google')}
 			>
 				<Google />
 				Google
 			</Button>
 			<Button
 				className="gap-2"
-				onClick={() =>
-					signIn('github', {
-						callbackUrl: `${window.location.origin}`,
-					})
-				}
+				onClick={() => socialLogin('github')}
 			>
 				<Github />
 				Github
