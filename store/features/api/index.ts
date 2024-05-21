4a692ddac3f63719b2cc221fcc853535b490a49d
Tareq <tareqnmd@gmail.com>
2024-05-22 00:21:46 +0600

    nb

--- store/features/api/index.ts
+++ store/features/api/index.ts
@@ -4,6 +4,9 @@ import { signOut } from 'next-auth/react';
 
 const baseQuery = fetchBaseQuery({
 	baseUrl: process.env.NEXT_PUBLIC_API_URL,
+	prepareHeaders(headers) {
+		return headers;
+	},
 	credentials: 'include',
 });
 
