7e320dde7e3cff77dd2540537ae208c19b5780cb
Tareq <tareqnmd@gmail.com>
2024-05-22 19:05:35 +0600

    orgi

--- store/features/api/index.ts
+++ store/features/api/index.ts
@@ -3,9 +3,8 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { signOut } from 'next-auth/react';
 
 const baseQuery = fetchBaseQuery({
-	baseUrl: `${process.env.NEXT_PUBLIC_API_URL}`,
+	baseUrl: process.env.NEXT_PUBLIC_API_URL,
 	credentials: 'include',
-	// mode: 'cors',
 });
 
 const apiSlice = createApi({
