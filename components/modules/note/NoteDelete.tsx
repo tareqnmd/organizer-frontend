815237a9c929f9ede7a4e0d283c67fc780132df5
Tareq <tareqnmd@gmail.com>
2024-05-29 15:41:56 +0600

    delete

--- components/modules/note/NoteDelete.tsx
+++ components/modules/note/NoteDelete.tsx
@@ -1,7 +1,77 @@
-import { NoteType } from "@/types/modules/note/budget-note-types";
-
+'use client';
+import ErrorMessage from '@/components/common/message/ErrorMessage';
+import SuccessMessage from '@/components/common/message/SuccessMessage';
+import { Button } from '@/components/ui/button';
+import {
+	Dialog,
+	DialogContent,
+	DialogFooter,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from '@/components/ui/dialog';
+import { getError } from '@/lib/helper/common';
+import { useDeleteNoteMutation } from '@/store/features/note/api';
+import { NoteType } from '@/types/modules/note/budget-note-types';
+import { Loader, Trash } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import { toast } from 'sonner';
 const NoteDelete = ({ note }: { note: NoteType }) => {
-	return <></>;
+	const [open, setOpen] = useState(false);
+	const router = useRouter();
+	const [deleteNote, { isLoading, isError, isSuccess, error }] =
+		useDeleteNoteMutation();
+	const deleteHandler = () => {
+		deleteNote(note.id);
+	};
+	useEffect(() => {
+		if (isError || isSuccess) {
+			toast(
+				isError ? (
+					<ErrorMessage message={getError(error)} />
+				) : (
+					<SuccessMessage message={`Type successfully deleted`} />
+				)
+			);
+			setOpen(false);
+			router.refresh();
+		}
+	}, [error, isError, isSuccess, router]);
+	return (
+		<Dialog
+			open={open}
+			onOpenChange={setOpen}
+		>
+			<DialogTrigger asChild>
+				<Trash
+					className="cursor-pointer"
+					size={16}
+				/>
+			</DialogTrigger>
+			<DialogContent className="sm:max-w-[425px]">
+				<DialogHeader>
+					<DialogTitle>Delete Note ({note.subject})</DialogTitle>
+				</DialogHeader>
+				Do you want to delete the note?
+				<DialogFooter>
+					<Button
+						onClick={deleteHandler}
+						className="bg-red-900 flex items-center gap-1"
+						disabled={isLoading}
+					>
+						{isLoading ? (
+							<Loader
+								className="animate-spin"
+								size={16}
+							/>
+						) : null}
+						Delete
+					</Button>
+				</DialogFooter>
+			</DialogContent>
+		</Dialog>
+	);
 };
 
 export default NoteDelete;
