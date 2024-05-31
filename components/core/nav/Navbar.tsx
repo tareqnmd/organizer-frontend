74190b815ee16f67ef267fbecf721f8fba25796d
Tareq <tareqnmd@gmail.com>
2024-05-31 13:15:42 +0600

    nav footer

--- components/core/nav/Navbar.tsx
+++ components/core/nav/Navbar.tsx
@@ -4,7 +4,7 @@ import Links from './Links';
 
 const Navbar = () => {
 	return (
-		<nav className="sticky top-0 z-50 shadow bg-white transition duration-500 ">
+		<nav className="sticky top-0 z-50 shadow bg-white transition duration-500 w-screen">
 			<div className="container py-4 mx-auto flex flex-wrap items-center justify-between">
 				<Link href="/">
 					<Logo />
