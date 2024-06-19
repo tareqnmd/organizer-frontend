00ff0c573ef951783f6f8bdaf57e6474754f55b6
Tareq <tareqnmd@gmail.com>
2024-06-19 22:03:19 +0600

    update

--- components/modules/budget/transaction/BudgetTransactionFilter.tsx
+++ components/modules/budget/transaction/BudgetTransactionFilter.tsx
@@ -2,64 +2,17 @@
 import FormDateRange from '@/components/common/input/DateRange';
 import FormSelect from '@/components/common/input/Select';
 import { Input } from '@/components/ui/input';
-import { useDebounce } from '@/hooks/useDebounce';
-import { toQueryString } from '@/lib/helper/common';
-import { baseDateFormat } from '@/lib/helper/date';
 import { BudgetTransactionParamType } from '@/types/modules/budget/budget-transaction-types';
-import { useRouter } from 'next/navigation';
-import { useEffect, useRef, useState } from 'react';
 
 const BudgetTransactionFilter = ({
-	searchParams,
+	filterData,
+	onChange,
+	onDateRangeUpdate,
 }: {
-	searchParams: BudgetTransactionParamType;
+	filterData: BudgetTransactionParamType;
+	onChange: (value: string, name: string) => void;
+	onDateRangeUpdate: (value: { from: Date; to: Date }) => void;
 }) => {
-	const router = useRouter();
-	const hasRendered = useRef(false);
-	const [filterData, setFilterData] = useState({
-		category: searchParams.category ?? '',
-		type: searchParams.type ?? '',
-		transaction: searchParams.transaction ?? '',
-		from: searchParams.from ?? '',
-		to: searchParams.to ?? '',
-	});
-	const debouncedText = useDebounce(filterData.transaction, 500);
-
-	const changeHandler = (value: string, name: string) => {
-		setFilterData((prev) => ({ ...prev, [name]: value }));
-	};
-
-	useEffect(() => {
-		if (hasRendered.current) {
-			router.push(
-				`/budget/transaction${toQueryString({
-					type: filterData.type,
-					category: filterData.category,
-					transaction: debouncedText,
-					from: filterData.from,
-					to: filterData.to,
-				})}`
-			);
-		} else {
-			hasRendered.current = true;
-		}
-	}, [
-		debouncedText,
-		filterData.type,
-		filterData.category,
-		filterData.from,
-		filterData.to,
-		router,
-	]);
-
-	const dateRangeUpdate = (value: { from: Date; to: Date }) => {
-		setFilterData((prev) => ({
-			...prev,
-			from: value?.from ? baseDateFormat(value.from) : '',
-			to: value?.to ? baseDateFormat(value.to) : '',
-		}));
-	};
-
 	return (
 		<>
 			<div className="flex gap-2 col-span-4">
@@ -67,7 +20,7 @@ const BudgetTransactionFilter = ({
 					className="h-8"
 					placeholder="Search By Description"
 					value={filterData.transaction}
-					onChange={(e) => changeHandler(e.target.value, 'transaction')}
+					onChange={(e) => onChange(e.target.value, 'transaction')}
 				/>
 
 				<FormSelect
@@ -78,7 +31,7 @@ const BudgetTransactionFilter = ({
 						optionUrl: '/budget/type-select',
 					}}
 					field={{
-						onChange: (value: string) => changeHandler(value, 'type'),
+						onChange: (value: string) => onChange(value, 'type'),
 						value: filterData.type,
 					}}
 				/>
@@ -90,13 +43,13 @@ const BudgetTransactionFilter = ({
 						optionUrl: '/budget/type-category-select',
 					}}
 					field={{
-						onChange: (value: string) => changeHandler(value, 'category'),
+						onChange: (value: string) => onChange(value, 'category'),
 						value: filterData.category,
 					}}
 				/>
 			</div>
 			<FormDateRange
-				onUpdate={dateRangeUpdate}
+				onUpdate={onDateRangeUpdate}
 				triggerClassName="h-8"
 				className="w-full col-span-3 xl:col-span-1 xl:col-start-3"
 				initialValues={{
--- components/modules/budget/transaction/BudgetTransactionsWrapper.tsx
+++ components/modules/budget/transaction/BudgetTransactionsWrapper.tsx
@@ -1,5 +1,10 @@
+'use client';
+import { useDebounce } from '@/hooks/useDebounce';
+import { toQueryString } from '@/lib/helper/common';
+import { baseDateFormat } from '@/lib/helper/date';
 import { BudgetTransactionParamType } from '@/types/modules/budget/budget-transaction-types';
-import { ReactNode } from 'react';
+import { useRouter } from 'next/navigation';
+import { ReactNode, useEffect, useRef, useState } from 'react';
 import BudgetTransactionAdd from './BudgetTransactionAdd';
 import BudgetTransactionFilter from './BudgetTransactionFilter';
 import BudgetTransactionPagination from './BudgetTransactionPagination';
@@ -11,10 +16,59 @@ const BudgetTransactionsWrapper = ({
 	children: ReactNode;
 	searchOptions: BudgetTransactionParamType;
 }) => {
+	const router = useRouter();
+	const hasRendered = useRef(false);
+	const [filterData, setFilterData] = useState({
+		category: searchOptions.category ?? '',
+		type: searchOptions.type ?? '',
+		transaction: searchOptions.transaction ?? '',
+		from: searchOptions.from ?? '',
+		to: searchOptions.to ?? '',
+	});
+	const debouncedText = useDebounce(filterData.transaction, 500);
+
+	const changeHandler = (value: string, name: string) => {
+		setFilterData((prev) => ({ ...prev, [name]: value }));
+	};
+
+	useEffect(() => {
+		if (hasRendered.current) {
+			router.push(
+				`/budget/transaction${toQueryString({
+					type: filterData.type,
+					category: filterData.category,
+					transaction: debouncedText,
+					from: filterData.from,
+					to: filterData.to,
+				})}`
+			);
+		} else {
+			hasRendered.current = true;
+		}
+	}, [
+		debouncedText,
+		filterData.type,
+		filterData.category,
+		filterData.from,
+		filterData.to,
+		router,
+	]);
+
+	const dateRangeUpdate = (value: { from: Date; to: Date }) => {
+		setFilterData((prev) => ({
+			...prev,
+			from: value?.from ? baseDateFormat(value.from) : '',
+			to: value?.to ? baseDateFormat(value.to) : '',
+		}));
+	};
 	return (
 		<div className="grid gap-4">
 			<div className="grid grid-cols-4 gap-2">
-				<BudgetTransactionFilter searchParams={searchOptions} />
+				<BudgetTransactionFilter
+					filterData={filterData}
+					onChange={changeHandler}
+					onDateRangeUpdate={dateRangeUpdate}
+				/>
 				<BudgetTransactionAdd />
 			</div>
 			{children}
