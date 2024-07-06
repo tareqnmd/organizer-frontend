4936755467e4ab0a469fa5947fbd418ae42e56d0
Tareq <tareqnmd@gmail.com>
2024-07-06 20:02:04 +0600

    up

--- components/core/NonPrivileged.tsx
+++ components/core/NonPrivileged.tsx
@@ -5,7 +5,12 @@ const NonPrivileged = () => {
 		<div className="grid place-content-center h-full">
 			<div className="text-center">
 				<h2>You are not admin!!</h2>
-				<Link href="/">Home</Link>
+				<Link
+					className="text-blue-500 underline"
+					href="/"
+				>
+					Home
+				</Link>
 			</div>
 		</div>
 	);
