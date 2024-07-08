2a9a048578ca02bc42a5a41f3a4432159b710bf1
Tareq <tareqnmd@gmail.com>
2024-07-08 22:28:26 +0600

    update

--- components/core/nav/UserOptions.tsx
+++ components/core/nav/UserOptions.tsx
@@ -14,13 +14,13 @@ export function UserOptions({ image }: { image: string }) {
 			<DropdownMenuTrigger asChild>
 				<Avatar className="cursor-pointer h-6 w-6 select-none">
 					<AvatarImage src={image} />
-					<AvatarFallback>MD</AvatarFallback>
+					<AvatarFallback></AvatarFallback>
 				</Avatar>
 			</DropdownMenuTrigger>
 			<DropdownMenuContent align="end">
 				<DropdownMenuItem asChild>
 					<Link
-						className="w-full cursor-pointer transition hover:underline"
+						className="w-full cursor-pointer transition hover:font-semibold"
 						href="/profile"
 					>
 						Profile
