bee5a34734ae4dbb47dd9d7e4b487423580b4900
Tareq <tareqnmd@gmail.com>
2024-10-07 22:58:20 +0600

    kanban data updated

--- components/modules/task/BoardList.tsx
+++ components/modules/task/BoardList.tsx
@@ -2,7 +2,7 @@ import { SortableContext, useSortable } from '@dnd-kit/sortable';
 import { CSS } from '@dnd-kit/utilities';
 import { Grip } from 'lucide-react';
 import BoardListCard from './BoardListCard';
-const BoardList = ({ list }: any) => {
+const BoardList = ({ list, listCards }: any) => {
 	const {
 		listeners,
 		transform,
@@ -16,13 +16,14 @@ const BoardList = ({ list }: any) => {
 		transformOrigin: 'center',
 		transform: CSS.Transform.toString(transform),
 		touchAction: 'none',
+		zIndex: isDragging ? 50 : 0,
 	};
 	return (
 		<div
 			ref={setNodeRef}
 			key={list.id}
 			style={style}
-			className={`w-[280px] bg-white relative ${isDragging ? 'z-20' : 'z-0'}`}
+			className={`w-[280px] bg-white relative`}
 		>
 			<div className={`border shadow rounded p-2`}>
 				<div className="flex items-center justify-between m-2">
@@ -38,8 +39,8 @@ const BoardList = ({ list }: any) => {
 					</button>
 				</div>
 				<div className="flex flex-col gap-2">
-					<SortableContext items={list.cards}>
-						{list?.cards.map((card: any) => (
+					<SortableContext items={listCards}>
+						{listCards?.map((card: any) => (
 							<BoardListCard
 								key={card.id}
 								card={card}
--- components/modules/task/BoardView.tsx
+++ components/modules/task/BoardView.tsx
@@ -1,67 +1,109 @@
 'use client';
 
 import DnDContextLayout from '@/components/layout/DnDContextLayout';
-import { TASK_DATA } from '@/lib/task-data';
-import { DragEndEvent, DragStartEvent } from '@dnd-kit/core';
+import { getListTaskData, TASK_DATA } from '@/lib/dnd';
+import { DragEndEvent, DragOverEvent } from '@dnd-kit/core';
 import { arrayMove, SortableContext } from '@dnd-kit/sortable';
 import { useState } from 'react';
 import BoardList from './BoardList';
 
 const BoardView = () => {
-	const [lists, setLists] = useState(TASK_DATA);
-	const [activeList, setActiveList] = useState(null);
-	const [activeTask, setActiveTask] = useState(null);
+	const { listItems = [], cardItems = [] } = getListTaskData(TASK_DATA as any);
 
-	function onDragStart(event: DragStartEvent) {
-		const current: any = event?.active?.data?.current ?? '';
-		const type = event?.active?.data?.current?.type ?? '';
-		if (type === 'List') {
-			setActiveList(current?.list ?? {});
-			return;
-		} else if (type === 'Card') {
-			setActiveTask(current?.card ?? {});
-			return;
-		}
-	}
+	const [lists, setLists] = useState(listItems);
+	const [cards, setCards] = useState(cardItems);
 
 	const onDragEnd = (event: DragEndEvent) => {
-		const { active, over } = event;
-		const activeId = String(active.id);
-		const overId = over ? String(over.id) : null;
-		const type = active.data.current?.type;
-		if (activeId === overId) {
-			return;
-		}
-		if (type === 'List') {
-			setLists((users) => {
-				const itemOriginalPos = users.findIndex(
-					(item) => item.id === active.id
+		try {
+			const { active, over } = event;
+			if (!over) return;
+
+			const activeId = active.id;
+			const overId = over.id;
+
+			if (activeId === overId) return;
+
+			const isActiveAColumn = active.data.current?.type === 'List';
+			if (!isActiveAColumn) return;
+			setLists((lists: any) => {
+				const activeColumnIndex = lists.findIndex(
+					(list: any) => list.id === activeId
 				);
-				const itemNewPos = users.findIndex((item) => item.id === over?.id);
-				return arrayMove(users, itemOriginalPos, itemNewPos);
+				const overColumnIndex = lists.findIndex(
+					(list: any) => list.id === overId
+				);
+
+				return arrayMove(lists, activeColumnIndex, overColumnIndex);
 			});
-		} else if (type === 'Card') {
+		} catch (e: any) {
+			console.log('e', e);
 		}
 	};
+
+	function onDragOver(event: DragOverEvent) {
+		try {
+			const { active, over } = event;
+			if (!over) return;
+
+			const activeId = active.id;
+			const overId = over.id;
+
+			if (activeId === overId) return;
+
+			const isActiveATask = active.data.current?.type === 'Card';
+			const isOverATask = over.data.current?.type === 'Card';
+
+			if (!isActiveATask) return;
+
+			if (isActiveATask && isOverATask) {
+				setCards((cards: any) => {
+					const activeIndex = cards.findIndex(
+						(card: any) => card.id === activeId
+					);
+					const overIndex = cards.findIndex((card: any) => card.id === overId);
+					if (cards[activeIndex].listId !== cards[overIndex].listId) {
+						cards[activeIndex].listId = cards[overIndex].listId;
+						return arrayMove(cards, activeIndex, overIndex - 1);
+					}
+					return arrayMove(cards, activeIndex, overIndex);
+				});
+			}
+
+			const isOverAColumn = over.data.current?.type === 'List';
+
+			if (isActiveATask && isOverAColumn) {
+				setCards((cards: any) => {
+					const activeIndex = cards.findIndex(
+						(card: any) => card.id === activeId
+					);
+					if (cards[activeIndex]?.listId) {
+						cards[activeIndex].listId = overId;
+					}
+					return arrayMove(cards, activeIndex, activeIndex);
+				});
+			}
+		} catch (e: any) {
+			console.log('e', e);
+		}
+	}
 
 	return (
 		<div className="w-full overflow-x-auto h-full">
 			<DnDContextLayout
-				handleDragStart={onDragStart}
+				handleDragOver={onDragOver}
 				handleDragEnd={onDragEnd}
 			>
-				<SortableContext items={lists}>
-					<div className="flex gap-2 w-max">
-						<SortableContext items={lists}>
-							{lists.map((list: any) => (
-								<BoardList
-									list={list}
-									key={list.id}
-								/>
-							))}
-						</SortableContext>
-					</div>
-				</SortableContext>
+				<div className="flex gap-2 w-max">
+					<SortableContext items={lists}>
+						{lists?.map((list: any) => (
+							<BoardList
+								list={list}
+								key={list.id}
+								listCards={cards.filter((card: any) => card.listId === list.id)}
+							/>
+						))}
+					</SortableContext>
+				</div>
 			</DnDContextLayout>
 		</div>
 	);
