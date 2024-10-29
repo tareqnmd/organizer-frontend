93d54de7cd5cfc47c872921eb83d8610ca064327
Tareq <tareqnmd@gmail.com>
2024-10-29 23:11:59 +0600

    module layout fix

--- components/dashboard/Dashboard.tsx
+++ components/dashboard/Dashboard.tsx
@@ -3,7 +3,7 @@ import Module from './Module';
 
 const Dashboard = ({ modules }: { modules: RouteType[] }) => {
 	return (
-		<div className="m-auto grid h-full max-w-[800px] grid-cols-1 place-content-center gap-4 sm:grid-cols-2 lg:grid-cols-3">
+		<div className="m-auto grid h-full max-w-screen-xl grid-cols-1 place-content-center gap-4 sm:grid-cols-2 lg:grid-cols-3">
 			{modules.map((module) => (
 				<Module key={module.path} module={module} />
 			))}
