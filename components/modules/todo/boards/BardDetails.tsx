16bd43e8063584ba677f22b8074c483e4f691fee
Tareq <tareqnmd@gmail.com>
2024-11-04 22:06:40 +0600

    update

--- components/modules/todo/boards/BardDetails.tsx
+++ components/modules/todo/boards/BardDetails.tsx
@@ -3,9 +3,9 @@ import { baseDateFormat } from '@/lib/utils';
 
 const BoardDetails = ({ board }: { board: BoardType }) => {
 	return (
-		<div className="flex flex-col border-b py-2 text-lg font-bold">
+		<div className="z-10 flex flex-col border-b py-2 text-lg font-bold text-white">
 			{board?.title}
-			<div className="text-sm text-gray-500">
+			<div className="text-sm text-gray-300">
 				created by {baseDateFormat(board?.createdAt)}
 			</div>
 		</div>
