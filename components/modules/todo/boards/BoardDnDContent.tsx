52150dad26f8df276446f7450817b2af4d0ad7c9
Tareq <tareqnmd@gmail.com>
2024-11-05 07:49:35 +0600

    update

--- components/modules/todo/boards/BoardDnDContent.tsx
+++ components/modules/todo/boards/BoardDnDContent.tsx
@@ -3,7 +3,7 @@
 import DnDContextLayout from '@/components/layout/DnDContextLayout';
 import { CardType, ListType } from '@/lib/helper/todo';
 import { DragEndEvent, DragOverEvent } from '@dnd-kit/core';
-import { SortableContext } from '@dnd-kit/sortable';
+import { arrayMove, SortableContext } from '@dnd-kit/sortable';
 import { useState } from 'react';
 import BoardList from '../list/BoardList';
 import BoardListForm from '../list/BoardListForm';
@@ -20,12 +20,82 @@ const BoardDnDContent = ({
 	const [lists, setLists] = useState(listItems);
 	const [cards, setCards] = useState(cardItems);
 
-	const onDragOver = (event: DragOverEvent) => {
-		console.log(event);
-	};
 	const onDragEnd = (event: DragEndEvent) => {
-		console.log(event);
+		try {
+			const { active, over } = event;
+			if (!over) return;
+
+			const activeId = active.id;
+			const overId = over.id;
+
+			if (activeId === overId) return;
+
+			const isActiveAColumn = active.data.current?.type === 'List';
+			if (!isActiveAColumn) return;
+			setLists((lists: any) => {
+				const activeColumnIndex = lists.findIndex(
+					(list: any) => list.id === activeId,
+				);
+				const overColumnIndex = lists.findIndex(
+					(list: any) => list.id === overId,
+				);
+
+				return arrayMove(lists, activeColumnIndex, overColumnIndex);
+			});
+		} catch (e: any) {
+			console.log('e', e);
+		}
 	};
+
+	function onDragOver(event: DragOverEvent) {
+		try {
+			const { active, over } = event;
+			if (!over) return;
+
+			const activeId = active.id;
+			const overId = over.id;
+
+			if (activeId === overId) return;
+
+			const isActiveATask = active.data.current?.type === 'Card';
+			const isOverATask = over.data.current?.type === 'Card';
+
+			if (!isActiveATask) return;
+
+			if (isActiveATask && isOverATask) {
+				setCards((cards: any) => {
+					const activeIndex = cards.findIndex(
+						(card: any) => card.id === activeId,
+					);
+					const overIndex = cards.findIndex(
+						(card: any) => card.id === overId,
+					);
+					if (cards[activeIndex].listId !== cards[overIndex].listId) {
+						cards[activeIndex].listId = cards[overIndex].listId;
+						return arrayMove(cards, activeIndex, overIndex - 1);
+					}
+					return arrayMove(cards, activeIndex, overIndex);
+				});
+			}
+
+			const isOverAColumn = over.data.current?.type === 'List';
+
+			if (isActiveATask && isOverAColumn) {
+				setCards((cards: any) => {
+					const activeIndex = cards.findIndex(
+						(card: any) => card.id === activeId,
+					);
+					if (cards[activeIndex]?.listId) {
+						cards[activeIndex].listId = overId;
+					}
+					return arrayMove(cards, activeIndex, activeIndex);
+				});
+			}
+		} catch (e: any) {
+			console.log('e', e);
+		}
+	}
+
 	return (
 		<div className="z-10 h-full w-full overflow-x-auto">
 			<DnDContextLayout
