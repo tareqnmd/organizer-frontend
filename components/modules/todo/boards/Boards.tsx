1f14f493acc32e1cbfebbfe43cacc9c15ba24f76
Tareq <tareqnmd@gmail.com>
2024-11-16 12:51:15 +0600

    update

--- components/modules/todo/boards/Boards.tsx
+++ components/modules/todo/boards/Boards.tsx
@@ -1,14 +1,23 @@
+import { Card } from '@/components/ui/card';
 import BoardCard from './BoardCard';
 
 import { BoardType } from '@/lib/helper/todo';
 
 const Boards = ({ boards }: { boards: BoardType[] }) => {
 	return (
-		<div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
-			{boards.map((board: any) => (
-				<BoardCard key={board.id} board={board} />
-			))}
-		</div>
+		<>
+			{boards?.length > 0 ? (
+				<div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
+					{boards?.map((board: any) => (
+						<BoardCard key={board.id} board={board} />
+					))}
+				</div>
+			) : (
+				<Card className="basic-card relative grid min-h-[80px] place-content-center active:scale-100">
+					No boards found
+				</Card>
+			)}
+		</>
 	);
 };
 
--- components/modules/todo/workspaces/Workspaces.tsx
+++ components/modules/todo/workspaces/Workspaces.tsx
@@ -1,13 +1,25 @@
+import { Card } from '@/components/ui/card';
 import { WorkspaceType } from '@/lib/helper/todo';
 import WorkspaceCard from './WorkspaceCard';
 
 const Workspaces = ({ workspaces }: { workspaces: WorkspaceType[] }) => {
 	return (
-		<div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
-			{workspaces?.map((workspace: WorkspaceType) => (
-				<WorkspaceCard key={workspace.id} workspace={workspace} />
-			))}
-		</div>
+		<>
+			{workspaces?.length > 0 ? (
+				<div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
+					{workspaces?.map((workspace: WorkspaceType) => (
+						<WorkspaceCard
+							key={workspace.id}
+							workspace={workspace}
+						/>
+					))}
+				</div>
+			) : (
+				<Card className="basic-card relative grid min-h-[80px] place-content-center active:scale-100">
+					No workspaces found
+				</Card>
+			)}
+		</>
 	);
 };
 
