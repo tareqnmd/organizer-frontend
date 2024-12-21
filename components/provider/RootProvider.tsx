c65104e0e6ec3f150be86d5d36f6acaafa3b8de2
Tareq <tareqnmd@gmail.com>
2024-12-21 18:11:31 +0600

    feature: ga4 & gtm
    1. ga4 implemented
    2. gtm implemented

--- components/provider/RootProvider.tsx
+++ components/provider/RootProvider.tsx
@@ -3,35 +3,38 @@ import { Toaster } from '@/components/ui/sonner';
 import NextTopLoader from 'nextjs-toploader';
 import Footer from '../core/Footer';
 import Navbar from '../core/nav/Navbar';
+import AnalyticsProvider from './AnalyticsProvider';
 import AuthProvider from './AuthProvider';
 import RefreshSession from './RefreshSession';
 import { ThemeProvider } from './ThemeProvider';
 
 const RootProvider = ({ children }: { children: React.ReactNode }) => {
 	return (
-		<AuthProvider>
-			<ThemeProvider
-				attribute="class"
-				defaultTheme="system"
-				enableSystem
-				disableTransitionOnChange
-			>
-				<StoreProvider>
-					<Toaster richColors />
-					<RefreshSession />
-					<NextTopLoader
-						color="#219aca"
-						showSpinner={false}
-						shadow="none"
-					/>
-					<div id="organizer">
-						<Navbar />
-						{children}
-						<Footer />
-					</div>
-				</StoreProvider>
-			</ThemeProvider>
-		</AuthProvider>
+		<AnalyticsProvider>
+			<AuthProvider>
+				<ThemeProvider
+					attribute="class"
+					defaultTheme="system"
+					enableSystem
+					disableTransitionOnChange
+				>
+					<StoreProvider>
+						<Toaster richColors />
+						<RefreshSession />
+						<NextTopLoader
+							color="#219aca"
+							showSpinner={false}
+							shadow="none"
+						/>
+						<div id="organizer">
+							<Navbar />
+							{children}
+							<Footer />
+						</div>
+					</StoreProvider>
+				</ThemeProvider>
+			</AuthProvider>
+		</AnalyticsProvider>
 	);
 };
 
