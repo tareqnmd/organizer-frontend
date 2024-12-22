279da5775b032b3112012a3c7320e8f64a711b65
Tareq <tareqnmd@gmail.com>
2024-12-22 07:42:32 +0600

    fix: free api info
    1. free api info updated

--- components/modules/FreeAPIInfo.tsx
+++ components/modules/FreeAPIInfo.tsx
@@ -8,7 +8,7 @@ const FreeAPIInfo = ({
 	apiLink: string;
 }) => {
 	return (
-		<div className="rounded border border-dashed p-2 text-center text-sm">
+		<div className="rounded border border-dashed border-light-border/80 p-2 text-center text-sm text-dark/80 dark:border-dark-border/80 dark:text-light/80">
 			Using the free API from{' '}
 			<Link className="underline" target="_blank" href={apiLink}>
 				{apiName}
