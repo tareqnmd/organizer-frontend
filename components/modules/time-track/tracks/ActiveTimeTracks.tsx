a8954e230e6418d144fc575546003338308ea24c
Tareq <tareqnmd@gmail.com>
2024-12-25 21:45:20 +0600

    fix: time track layout
    1. time track layout updated

--- components/modules/time-track/tracks/ActiveTimeTracks.tsx
+++ components/modules/time-track/tracks/ActiveTimeTracks.tsx
@@ -2,15 +2,19 @@ import { getTimeTrackTracks, TimeTrackType } from '@/lib/helper/time-track';
 import TimeTrackCard from './TmeTrackCard';
 
 const ActiveTimeTracks = async () => {
-	const tracks = await getTimeTrackTracks({ isActive: true });
+	const tracks = await getTimeTrackTracks({ isActive: true, limit: 5 });
 	return (
 		<div className="basic-card flex flex-col gap-3 active:scale-100">
 			<strong className="font-semibold">Active Time Tracks</strong>
-			<div className="flex flex-col gap-2">
-				{tracks.map((track: TimeTrackType) => (
-					<TimeTrackCard key={track.id} track={track} />
-				))}
-			</div>
+			{tracks?.length > 0 ? (
+				<div className="flex flex-col gap-2">
+					{tracks.map((track: TimeTrackType) => (
+						<TimeTrackCard key={track.id} track={track} />
+					))}
+				</div>
+			) : (
+				<div className="text-sm">No active time tracks</div>
+			)}
 		</div>
 	);
 };
--- components/modules/time-track/tracks/RecentTimeTracks.tsx
+++ components/modules/time-track/tracks/RecentTimeTracks.tsx
@@ -6,11 +6,15 @@ const RecentTimeTracks = async () => {
 	return (
 		<div className="basic-card flex flex-col gap-3 active:scale-100">
 			<strong className="font-semibold">Recent Time Tracks</strong>
-			<div className="flex flex-col gap-2">
-				{tracks.map((track: TimeTrackType) => (
-					<TimeTrackCard key={track.id} track={track} />
-				))}
-			</div>
+			{tracks?.length > 0 ? (
+				<div className="flex flex-col gap-2">
+					{tracks.map((track: TimeTrackType) => (
+						<TimeTrackCard key={track.id} track={track} />
+					))}
+				</div>
+			) : (
+				<div className="text-sm">No recent time tracks</div>
+			)}
 		</div>
 	);
 };
