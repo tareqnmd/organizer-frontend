5f8aae4e61ccf50eed0daa69637b1b00bab32d28
Tareq <tareqnmd@gmail.com>
2025-04-12 14:18:51 +0600

    refactor: update Radio and BudgetStatsCards components for improved layout
    - Adjusted the Radio component's class to use "flex-col gap-2" for better spacing.
    - Modified the BudgetStatsCards component's grid layout to use "md:grid-cols-3" for improved responsiveness.

--- components/modules/budget/dashboard/overview/BudgetStatsCards.tsx
+++ components/modules/budget/dashboard/overview/BudgetStatsCards.tsx
@@ -30,7 +30,7 @@ function BudgetStatsCards({
 	} = amount || {};
 
 	return (
-		<div className="grid grid-cols-2 gap-4 lg:grid-cols-6">
+		<div className="grid grid-cols-2 gap-4 md:grid-cols-3">
 			<SkeletonWrapper isLoading={false}>
 				<BudgetStatsCard
 					formatter={formatter}
